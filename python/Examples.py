94f9548db10690ae94435c254376fc575621d4b0
damast93 <damast93@aol.com>
2021-04-20 19:53:15 +0200

    condensed examples

--- python/Examples.py
+++ python/Examples.py
@@ -1,7 +1,9 @@
+import numpy as np
 import matplotlib.pyplot as plt
+
 from Infer import Infer
 
-#%% Condition on some equalities
+#%% Condition on equality
 
 # Model:
 #   X, Y, Z ~ N(0,1) iid
@@ -119,40 +121,38 @@ ymean = [ g.mean(yi) for yi in y ]
 plt.plot(xmean, ymean, 'b-')
 plt.show()
 
-#%% A distribution over closed loops
+#%% Gaussian process example
+
+import Gaussian
 
 g = Infer()
 
-N = 25
-dt = 1
+# rbf kernel
+rbf = lambda x,y: 0.2 * np.exp(-(x-y)**2)
 
-x, y = [0] * N, [0] * N
-vx, vy = [0] * N, [0] * N
+def gp(xs, kernel):
+    K = [[ kernel(x1,x2) for x1 in xs] for x2 in xs ]
+    return Gaussian.N(np.array(K))
 
-x[0] = 0.0 + g.N(0, 0.01)
-y[0] = 0.0 + g.N(0, 0.01)
+# build GP prior
+xs = np.linspace(-5, 5, 100)
+ys = g.from_dist(gp(xs, rbf))
 
-# simulate some trajectories
-for i in range(1, N):
-    ax = g.N(0, 0.01)
-    ay = g.N(0, 0.01)
+# add observations
+observations = [(20,1.0), (40, 2.0), (60, 3.0), (80, 0.0)]
+for (i,yobs) in observations:
+    g.condition(ys[i],yobs)
+
+# plot sample functions
+posterior = g.marginals(*ys)
+for i in range(750):
+    samples = posterior.sample()
+    plt.plot(xs,samples,alpha=0.05)
     
-    vx[i] = vx[i-1] + ax*dt
-    vy[i] = vy[i-1] + ay*dt
-    
-    x[i] = x[i-1] + vx[i]*dt
-    y[i] = y[i-1] + vy[i]*dt
+# Plot means
+means = np.array([ g.mean(y) for y in ys ])
+plt.plot(xs, means, 'red')
 
-# condition that trajectories return to their origin
-g.condition(x[N-1], x[0])
-g.condition(y[N-1], y[0])
-
-trajectory = g.marginals(*(x + y))
-for i in range(1,25):
-    vals = trajectory.sample()
-    xs,ys = vals[0:N], vals[N:]
-    
-    plt.plot(xs, ys, '-', alpha=0.25)
-
-plt.show()
-    
+# Standard deviation ± 3sigma
+stds = np.array([ np.sqrt(np.abs(g.variance(y))) for y in ys ])
+plt.plot(xs, means + 3*stds, 'blue', xs, means - 3*stds, 'blue')
